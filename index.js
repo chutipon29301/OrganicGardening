b2022a24fa6962facc3e13ec3c8eb38b8c03d4e9
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-03-22 09:45:11 +0700

    Change port

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var path = require('path');
 var app = express();
-app.listen(process.env.PORT | 3000);
-
-app.use(express.static('public'));
+app.listen(process.env.PORT | 3000, _ => {
+    console.log('Listening on port ' + (process.env.PORT | 3000));
+});
+app.use(express.static(path.join(__dirname, 'public')));
