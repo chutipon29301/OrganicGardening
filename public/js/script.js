57c6446c945d9f218da7ce28bb889a14466778be
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-03-22 09:30:56 +0700

    Add many files

---
+++ public/js/script.js
@@ -0,0 +1,43 @@
+$('#topic2').click(function() {
+    window.open('Lectures/2.pdf','_blank');
+});
+
+$('#topic3').click(function() {
+    window.open('Lectures/3.pdf','_blank');
+});
+
+$('#topic4').click(function() {
+    window.open('Lectures/4.pdf','_blank');
+});
+
+$('#topic5_6').click(function() {
+    window.open('Lectures/5_6.pdf','_blank');
+});
+
+$('#topic7').click(function() {
+    window.open('Lectures/7.pdf','_blank');
+});
+
+$('#topic8').click(function() {
+    window.open('Lectures/temp.pdf','_blank');
+});
+
+$('#topic9').click(function() {
+    window.open('Lectures/temp.pdf','_blank');
+});
+
+$('#topic10').click(function() {
+    window.open('Lectures/10.pdf','_blank');
+});
+
+$('#topic11').click(function() {
+    window.open('Lectures/11.pdf','_blank');
+});
+
+$('#topic12').click(function() {
+    window.open('Lectures/12.pdf','_blank');
+});
+
+$('#topic13').click(function() {
+    window.open('Lectures/13.pdf','_blank');
+});
