bf5ccd2e949014207e3bc4fd3c276eb6ce3795f2
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-03-22 11:01:14 +0700

    Remove unused file

--- index.js
+++ index.js
@@ -5,7 +5,3 @@ app.listen(process.env.PORT || 3000, _ => {
     console.log('Listening on port ' + (process.env.PORT || 3000));
 });
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/',(req,res) => {
-    console.log('Called');
-    res.status(200).sendFile(path.join(__dirname, 'public/index.html'));
-});
