e609e89b31d9c253f35b880cf2adf9b9dda28dce
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-03-22 10:36:21 +0700

    Config port for delpoy

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-app.listen(process.env.PORT | 3000, _ => {
-    console.log('Listening on port ' + (process.env.PORT | 3000));
+app.listen(process.env.PORT || 3000, _ => {
+    console.log('Listening on port ' + (process.env.PORT || 3000));
 });
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/',(req,res) => {
